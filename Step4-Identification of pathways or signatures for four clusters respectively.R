e131310bb2fcde3fa67f24396430e771c25bcbf8
KangWang0822 <49832670+KangWang0822@users.noreply.github.com>
2021-01-06 22:12:30 +0100

    Update Step4-Identification of pathways or signatures for four clusters respectively.R

--- Step4-Identification of pathways or signatures for four clusters respectively.R
+++ Step4-Identification of pathways or signatures for four clusters respectively.R
@@ -27,8 +27,12 @@ immun=msigdbr(species="Homo sapiens",category=c("C7"))%>%select(gs_name,gene_sym
 #exp=exp[cg,]
 #write.csv(exp,file="F:/Ph.D projects/1.PROMIX_10y_followup/dataprocessing/step4/GSEA/exp.csv")
 #write.csv(pheno,file="F:/Ph.D projects/1.PROMIX_10y_followup/dataprocessing/step4/GSEA/pheno.csv")
-########Cluster1 features##################immune
+###########################################
+#################All#######################
+###########################################
+########Cluster1 features##################cell cycle/immune
 library(limma);library(Glimma);library(edgeR);library(clusterProfiler)
+table(pheno$cluster4)
 pheno$group="cluster"
 pheno$group[pheno$cluster4%in%c("2","3","4")]="control"
 design<-model.matrix(~0+as.factor(pheno$group)+as.numeric(pheno$TumorPurity)+as.factor(pheno$IHC_subtype))
@@ -43,14 +47,14 @@ contr.matrix
 vfit <- lmFit(exp, design)
 vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
 efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
+DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.5,number=50000,sort.by = 'logFC')
 genelist=DGE%>%filter(logFC>0)
 genelist=row.names(genelist)
-GSEA_cluster1=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.05,
+GSEA_cluster1=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,minGSSize = 5,pvalueCutoff = 0.05,
                                     pAdjustMethod = "BH"))
-GSEA_cluster1_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                     pAdjustMethod = "BH"))
-########Cluster2 features##################代谢
+GSEA_cluster1$cluster="cluster1"
+GSEA_cluster1$group="All"
+########Cluster2 features##################metabolism
 pheno$group="cluster"
 pheno$group[pheno$cluster4%in%c("1","3","4")]="control"
 design<-model.matrix(~0+as.factor(pheno$group)+as.numeric(pheno$TumorPurity)+as.factor(pheno$IHC_subtype))
@@ -65,13 +69,14 @@ contr.matrix
 vfit <- lmFit(exp, design)
 vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
 efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
+DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.05,lfc=0.5,number=50000,sort.by = 'logFC')
 genelist=DGE%>%filter(logFC>0)
 genelist=row.names(genelist)
-GSEA_cluster2=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.05,
-                                     pAdjustMethod = "BH"))
-GSEA_cluster2_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
+GSEA_cluster2=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,minGSSize = 5,pvalueCutoff = 0.05,
+                                        pAdjustMethod = "BH"))
+
+GSEA_cluster2$cluster="cluster2"
+GSEA_cluster2$group="All"
 ########Cluster3 features##################EMT
 pheno$group="cluster"
 pheno$group[pheno$cluster4%in%c("1","2","4")]="control"
@@ -87,13 +92,13 @@ contr.matrix
 vfit <- lmFit(exp, design)
 vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
 efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
+DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.05,lfc=0.5,number=50000,sort.by = 'logFC')
 genelist=DGE%>%filter(logFC>0)
 genelist=row.names(genelist)
-GSEA_cluster3=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.05,
+GSEA_cluster3=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,minGSSize = 5,pvalueCutoff = 0.05,
                                      pAdjustMethod = "BH"))
-GSEA_cluster3_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
+GSEA_cluster3$cluster="cluster3"
+GSEA_cluster3$group="All"
 ########Cluster4 features##################cell-cycle
 pheno$group="cluster"
 pheno$group[pheno$cluster4%in%c("1","2","3")]="control"
@@ -109,113 +114,19 @@ contr.matrix
 vfit <- lmFit(exp, design)
 vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
 efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
+DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.05,lfc=0.5,number=50000,sort.by = 'logFC')
 genelist=DGE%>%filter(logFC>0)
 genelist=row.names(genelist)
-GSEA_cluster4=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.05,
-                                     pAdjustMethod = "BH"))
-GSEA_cluster4_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
+GSEA_cluster4=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,minGSSize = 5,pvalueCutoff = 0.05,
+                                        pAdjustMethod = "BH"))
 
-library(rJava);library(xlsxjars);library(xlsx);library(openxlsx)
-GSEA_results=Reduce(union,list(cluster1=GSEA_cluster1, ####得到后再次做差异分析！！！！genelist=final_genelist########
-                                 cluster2=GSEA_cluster2, 
-                                 cluster3=GSEA_cluster3,
-                                 cluster4=GSEA_cluster4))  
+GSEA_cluster4$cluster="cluster4"
+GSEA_cluster4$group="All"
+
+GSEA_ALL=rbind(GSEA_cluster1,GSEA_cluster2,GSEA_cluster3,GSEA_cluster4)
 ################################
 ############Output##############
 ################################
-mydata=list("cluster1"=GSEA_cluster1,"cluster2"=GSEA_cluster2,"cluster3"=GSEA_cluster3,"cluster4"=GSEA_cluster4)
-write.xlsx(mydata,file="F:/Ph.D projects/1.PROMIX_10y_followup/dataprocessing/step4/GSEA_results.xlsx")
-
-mydata=list("cluster1"=GSEA_cluster1_immun,"cluster2"=GSEA_cluster2_immun,"cluster3"=GSEA_cluster3_immun,"cluster4"=GSEA_cluster4_immun)
-write.xlsx(mydata,file="F:/Ph.D projects/1.PROMIX_10y_followup/dataprocessing/step4/GSEA_immune_results.xlsx")
-
-
-##########################################################
-###################3 cluster version######################
-##########################################################
-########Cluster2 features##################EMT
-library(limma);library(Glimma);library(edgeR);library(clusterProfiler)
-pheno$group="cluster"
-pheno$group[pheno$cluster3%in%c("2","3")]="control"
-design<-model.matrix(~0+as.factor(pheno$group)+as.numeric(pheno$TumorPurity)+as.factor(pheno$IHC_subtype))
-colnames(design)<-c("cluster","control","purity","subtype")
-design
-
-contr.matrix <- makeContrasts(
-  op.vs.baseline=cluster-control, 
-  levels=colnames(design))
-contr.matrix
-
-vfit <- lmFit(exp, design)
-vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
-efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
-genelist=DGE%>%filter(logFC>0)
-genelist=row.names(genelist)
-GSEA_cluster1=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.01,
-                                     pAdjustMethod = "BH"))
-GSEA_cluster1_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
-########Cluster2 features##################代谢
-pheno$group="cluster"
-pheno$group[pheno$cluster3%in%c("1","3")]="control"
-design<-model.matrix(~0+as.factor(pheno$group)+as.numeric(pheno$TumorPurity)+as.factor(pheno$IHC_subtype))
-colnames(design)<-c("cluster","control","purity","subtype")
-design
-
-contr.matrix <- makeContrasts(
-  op.vs.baseline=cluster-control, 
-  levels=colnames(design))
-contr.matrix
-
-vfit <- lmFit(exp, design)
-vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
-efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
-genelist=DGE%>%filter(logFC>0)
-genelist=row.names(genelist)
-GSEA_cluster2=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.01,
-                                     pAdjustMethod = "BH"))
-GSEA_cluster2_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
-########Cluster3 features##################EMT
-pheno$group="cluster"
-pheno$group[pheno$cluster3%in%c("1","2")]="control"
-design<-model.matrix(~0+as.factor(pheno$group)+as.numeric(pheno$TumorPurity)+as.factor(pheno$IHC_subtype))
-colnames(design)<-c("cluster","control","purity","subtype")
-design
-
-contr.matrix <- makeContrasts(
-  op.vs.baseline=cluster-control, 
-  levels=colnames(design))
-contr.matrix
-
-vfit <- lmFit(exp, design)
-vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
-efit <- eBayes(vfit)
-DGE = topTable(efit,adjust.method="BH",coef=1,p.value=0.01,lfc=0.58,number=50000,sort.by = 'logFC')
-genelist=DGE%>%filter(logFC>0)
-genelist=row.names(genelist)
-GSEA_cluster3=as.data.frame(enricher(gene=genelist,TERM2GENE=GSEA,pvalueCutoff = 0.01,
-                                     pAdjustMethod = "BH"))
-GSEA_cluster3_immun=as.data.frame(enricher(gene=genelist,TERM2GENE=immun,pvalueCutoff = 0.01,
-                                           pAdjustMethod = "BH"))
-
-###############################
-###############################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+library(rJava);library(xlsxjars);library(xlsx);library(openxlsx)
+#mydata=list("GSEA_ALL"=GSEA_ALL,"GSEA_Lum"=GSEA_Lum,"GSEA_TNBC"=GSEA_TNBC)
+write.xlsx(GSEA_ALL,file="F:/Ph.D projects/1.PROMIX_10y_followup/dataprocessing/step4/GSEA_results.xlsx")
